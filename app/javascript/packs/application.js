ba6bcafcf8ff9183e4c055d1c3c6c995e4108d45
Joshua Schultz <joshuamschultz@gmail.com>
2021-06-28 21:06:51 -0500

    small webpack change for jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import './src/application.scss'
+
+require("bootstrap/dist/js/bootstrap")
 
 
 Rails.start()
