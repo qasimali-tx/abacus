f18738e9a10bae132dee102e2721acd1e30d0e19
Usama Ashraf <usama.ashraf.khan@gmail.com>
2021-08-16 03:48:01 +0500

    Set up Puma, Nginx & Capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -35,7 +35,7 @@ set :linked_dirs, %w(tmp/pids tmp/sockets log)
 set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/master.key}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
