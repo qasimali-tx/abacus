dab22c0d95ec804c536da5df6bb3ec35c96069d2
Usama Ashraf <usama.ashraf.khan@gmail.com>
2021-08-15 20:48:55 +0500

    setting master as production branch for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :branch,        :master_copy
+set :branch,        :master
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
