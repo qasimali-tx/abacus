43ddec5297b9618b2ab1b0ee1ab56e5d84e35217
Usama Ashraf <usamaashrafkhan69@gmail.com>
2021-12-14 12:54:43 +0500

    resolve add account issue

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,15 @@
 class AccountsController < ApplicationController
+  include ApplicationHelper
+  before_action :check_login_name
+  before_action :yodlee_user_token
   def index
     @accounts = policy_scope(Account)
+  end
+
+  private
+  def check_login_name
+    if current_user.yodlee_login_name.blank?
+      current_user.update(yodlee_login_name: ENV["YODLEE_ADMIN_LOGOINNAME"])
+    end
   end
 end
